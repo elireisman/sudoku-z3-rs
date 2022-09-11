76782291f545a365725b23e6a37e20a75ccdc4e6
Eli Reisman <elireisman@github.com>
2022-09-11 16:34:06 -0700

    Declare problem and constraints in and frame up Z3 solver flow

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use std::collections::{HashMap, HashSet};
+use itertools::Itertools;
+use std::collections::HashMap;
 
 const BOARD_SIZE: usize = 9;
 
-#[derive(Debug, Clone, Eq, PartialEq, Hash, Copy)]
+#[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
 struct Pos {
     x: usize,
     y: usize,
@@ -48,52 +49,44 @@ impl<'ctx> SudokuModel<'ctx> {
 
     fn constrain_rows(&self, solver: &'ctx z3::Solver) {
         for y in 0..BOARD_SIZE {
-            let mut row = HashSet::<i64>::new();
-            for x in 0..BOARD_SIZE {
-                row.insert(
-                    self.board
-                        .get(&Pos { x, y })
-                        .unwrap()
-                        .value
-                        .as_i64()
-                        .unwrap_or(0),
-                );
-            }
-
-            for expected in 1..=BOARD_SIZE {
-                let expected = expected as i64;
-                solver.assert(&z3::ast::Bool::from_bool(self.ctx, row.contains(&expected)));
-                row.remove(&expected);
-            }
-            solver.assert(&z3::ast::Bool::from_bool(self.ctx, row.is_empty()));
+            let row_cells = self.get_row(y);
+            let pair_clauses = row_cells
+                .into_iter()
+                .combinations(2)
+                .map(|pair| {
+                    let lt = pair[0].lt(pair[1]);
+                    let gt = pair[0].gt(pair[1]);
+                    let clauses = vec![&lt, &gt];
+                    z3::ast::Bool::or(self.ctx, &clauses)
+                })
+                .collect::<Vec<_>>();
+            let assertions_expr =
+                z3::ast::Bool::and(self.ctx, &pair_clauses.iter().collect::<Vec<_>>());
+            solver.assert(&assertions_expr);
         }
     }
 
     fn constrain_cols(&self, solver: &z3::Solver) {
         for x in 0..BOARD_SIZE {
-            let mut col = HashSet::<i64>::new();
-            for y in 0..BOARD_SIZE {
-                col.insert(
-                    self.board
-                        .get(&Pos { x, y })
-                        .unwrap()
-                        .value
-                        .as_i64()
-                        .unwrap_or(0),
-                );
-            }
-
-            for expected in 1..=BOARD_SIZE {
-                let expected = expected as i64;
-                solver.assert(&z3::ast::Bool::from_bool(self.ctx, col.contains(&expected)));
-                col.remove(&expected);
-            }
-            solver.assert(&z3::ast::Bool::from_bool(self.ctx, col.is_empty()));
+            let col_cells = self.get_column(x);
+            let pair_clauses = col_cells
+                .into_iter()
+                .combinations(2)
+                .map(|pair: Vec<&'ctx z3::ast::Int<'_>>| {
+                    let lt = pair[0].lt(pair[1]);
+                    let gt = pair[0].gt(pair[1]);
+                    let clauses = vec![&lt, &gt];
+                    z3::ast::Bool::or(self.ctx, &clauses)
+                })
+                .collect::<Vec<_>>();
+            let assertions_expr =
+                z3::ast::Bool::and(self.ctx, &pair_clauses.iter().collect::<Vec<_>>());
+            solver.assert(&assertions_expr);
         }
     }
 
     fn constrain_cubes(&self, solver: &z3::Solver) {
-        for pos in vec![
+        for cube in vec![
             Pos { x: 0, y: 0 },
             Pos { x: 0, y: 3 },
             Pos { x: 0, y: 6 },
@@ -104,34 +97,55 @@ impl<'ctx> SudokuModel<'ctx> {
             Pos { x: 6, y: 3 },
             Pos { x: 6, y: 6 },
         ] {
-            self.constrain_cube(pos, solver);
+            let cube_cells = self.get_cube(cube);
+            let pair_clauses = cube_cells
+                .into_iter()
+                .combinations(2)
+                .map(|pair: Vec<&'ctx z3::ast::Int<'_>>| {
+                    let lt = pair[0].lt(pair[1]);
+                    let gt = pair[0].gt(pair[1]);
+                    let clauses = vec![&lt, &gt];
+                    z3::ast::Bool::or(self.ctx, &clauses)
+                })
+                .collect::<Vec<_>>();
+            let assertions_expr =
+                z3::ast::Bool::and(self.ctx, &pair_clauses.iter().collect::<Vec<_>>());
+            solver.assert(&assertions_expr);
         }
     }
 
-    fn constrain_cube(&self, top_left: Pos, solver: &z3::Solver) {
+    fn get_cube(&self, top_left: Pos) -> Vec<&'ctx z3::ast::Int> {
+        let mut cube: Vec<&'ctx z3::ast::Int> = vec![];
         for y in top_left.y..(top_left.y + 3) {
-            let mut cube = HashSet::<i64>::new();
             for x in top_left.x..(top_left.x + 3) {
-                cube.insert(
-                    self.board
-                        .get(&Pos { x, y })
-                        .unwrap()
-                        .value
-                        .as_i64()
-                        .unwrap_or(0),
-                );
+                let next_pos = Pos { x, y };
+                cube.push(&self.board.get(&next_pos).unwrap().value);
             }
-
-            for expected in 1..=BOARD_SIZE {
-                let expected = expected as i64;
-                solver.assert(&z3::ast::Bool::from_bool(
-                    self.ctx,
-                    cube.contains(&expected),
-                ));
-                cube.remove(&expected);
-            }
-            solver.assert(&z3::ast::Bool::from_bool(self.ctx, cube.is_empty()));
         }
+
+        cube
+    }
+
+    // obtain a Vec of references to all row members except the target
+    fn get_row(&self, target_y: usize) -> Vec<&'ctx z3::ast::Int> {
+        let mut row: Vec<&'ctx z3::ast::Int> = vec![];
+        for x in 0..BOARD_SIZE {
+            let next_pos = Pos { x: x, y: target_y };
+            row.push(&self.board.get(&next_pos).unwrap().value);
+        }
+
+        row
+    }
+
+    // obtain a Vec of references to all col members except the target
+    fn get_column(&self, target_x: usize) -> Vec<&'ctx z3::ast::Int> {
+        let mut col: Vec<&'ctx z3::ast::Int> = vec![];
+        for y in 0..BOARD_SIZE {
+            let next_pos = Pos { x: target_x, y: y };
+            col.push(&self.board.get(&next_pos).unwrap().value);
+        }
+
+        col
     }
 }
 

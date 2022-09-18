b9ea30725e190cbd371ccaa409025553ffeb18a8
Eli Reisman <elireisman@github.com>
2022-09-18 14:40:54 -0700

    nicer box

--- src/display/mod.rs
+++ src/display/mod.rs
@@ -8,23 +8,85 @@ pub const GREEN_FG: &str = "\x1b[32m";
 pub const BLUE_FG: &str = "\x1b[0;44m";
 pub const WHITE: &str = "\x1b[1;37m";
 
-fn horizontal() -> String {
-    format!("{}{}{}", WHITE, "+---------+---------+---------+\n", CLEAR)
+const TOP_RIGHT_CORNER: &str = "\x1b(0\x6b";
+const TOP_LEFT_CORNER: &str = "\x1b(0\x6c";
+const RIGHT_CORNER: &str = "\x1b(0\x75";
+const LEFT_CORNER: &str = "\x1b(0\x74";
+const BOTTOM_RIGHT_CORNER: &str = "\x1b(0\x6a";
+const BOTTOM_LEFT_CORNER: &str = "\x1b(0\x6d";
+const VERTICAL: &str = "\x1b(0\x78\x1b(0";
+const HORIZONTAL: &str = "\x1b(0\x71";
+const TOP_SPLIT: &str = "\x1b(0\x77";
+const BOTTOM_SPLIT: &str = "\x1b(0\x76";
+const EOL: &str = "\x1b(B\n";
+
+fn horizontal_top() -> String {
+    format!(
+        "{}{}{}",
+        WHITE,
+        vec![
+            TOP_LEFT_CORNER,
+            &HORIZONTAL.repeat(9),
+            TOP_SPLIT,
+            &HORIZONTAL.repeat(9),
+            TOP_SPLIT,
+            &HORIZONTAL.repeat(9),
+            TOP_RIGHT_CORNER,
+            EOL,
+        ]
+        .join(""),
+        CLEAR,
+    )
 }
 
-fn vertical() -> String {
-    format!("{}{}{}", WHITE, "|", CLEAR)
+fn horizontal() -> String {
+    format!(
+        "{}{}{}",
+        WHITE,
+        vec![
+            LEFT_CORNER,
+            &HORIZONTAL.repeat(9),
+            TOP_SPLIT,
+            &HORIZONTAL.repeat(9),
+            TOP_SPLIT,
+            &HORIZONTAL.repeat(9),
+            RIGHT_CORNER,
+            EOL,
+        ]
+        .join(""),
+        CLEAR,
+    )
+}
+
+fn horizontal_bottom() -> String {
+    format!(
+        "{}{}{}",
+        WHITE,
+        vec![
+            BOTTOM_LEFT_CORNER,
+            &HORIZONTAL.repeat(9),
+            BOTTOM_SPLIT,
+            &HORIZONTAL.repeat(9),
+            BOTTOM_SPLIT,
+            &HORIZONTAL.repeat(9),
+            BOTTOM_RIGHT_CORNER,
+            EOL,
+        ]
+        .join(""),
+        CLEAR,
+    )
 }
 
 pub fn from_input(input: &Vec<u8>) -> String {
     let mut out = String::new();
+    out.push_str(&horizontal_top());
     for y in 0..BOARD_SIZE {
-        if y % 3 == 0 {
+        if y == 3 || y == 6 {
             out.push_str(&horizontal());
         }
         for x in 0..BOARD_SIZE {
             if x % 3 == 0 {
-                out.push_str(&vertical());
+                out.push_str(&VERTICAL);
             }
 
             match input[(y * BOARD_SIZE) + x] as char {
@@ -38,24 +100,25 @@ pub fn from_input(input: &Vec<u8>) -> String {
                 }
             }
         }
-        out.push_str(&vertical());
+        out.push_str(&VERTICAL);
         out.push_str("\n");
     }
-    out.push_str(&horizontal());
+    out.push_str(&horizontal_bottom());
 
     out
 }
 
 pub fn from_model<'ctx>(model: &'ctx Model, solution: &'ctx z3::Model) -> String {
     let mut out = String::new();
+    out.push_str(&horizontal_top());
     for y in 0..BOARD_SIZE {
-        if y % 3 == 0 {
+        if y == 3 || y == 6 {
             out.push_str(&horizontal());
         }
 
         for x in 0..BOARD_SIZE {
             if x % 3 == 0 {
-                out.push_str(&vertical());
+                out.push_str(&VERTICAL);
             }
 
             let pos = Pos::new(x, y);
@@ -69,10 +132,10 @@ pub fn from_model<'ctx>(model: &'ctx Model, solution: &'ctx z3::Model) -> String
                 out.push_str(&elem);
             }
         }
-        out.push_str(&vertical());
+        out.push_str(&VERTICAL);
         out.push_str("\n");
     }
-    out.push_str(&horizontal());
+    out.push_str(&horizontal_bottom());
 
     out
 }
